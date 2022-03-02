537b0c689f5997c6e974f4d1525064ac73da27cc
DatBoi02 <92232433+DatBoi02@users.noreply.github.com>
2022-03-02 09:42:52 -0600

    Final Build y autores.

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class gameManager : MonoBehaviour
@@ -11,7 +12,8 @@ public class gameManager : MonoBehaviour
 
     [SerializeField] TextMeshProUGUI textQ;
     [SerializeField] TextMeshProUGUI textH;
-    [SerializeField] GameObject button;
+    [SerializeField] GameObject buttonE;
+    [SerializeField] GameObject buttonR;
 
     [SerializeField] TMP_InputField inputA;
 
@@ -21,6 +23,8 @@ public class gameManager : MonoBehaviour
         textQ.text = questions[i];
 
         savedWords = new string[questions.Length];
+
+        buttonR.SetActive(false);
     }
 
     public void saveAnswers ()
@@ -47,9 +51,27 @@ public class gameManager : MonoBehaviour
     {
         textH.gameObject.SetActive(true);
         textH.text = string.Format(historia, savedWords);
+        buttonR.SetActive(true);
 
         textQ.gameObject.SetActive(false);
-        button.SetActive(false);
+        buttonE.SetActive(false);
         inputA.gameObject.SetActive(false);
+    }
+
+    public void reiniciarJuego()
+    {
+        /*i = 0;
+        savedWords = new string[questions.Length];
+        textQ.text = questions[i];
+
+        textQ.gameObject.SetActive(true);
+        buttonE.SetActive(true);
+        inputA.gameObject.SetActive(true);
+
+        textH.gameObject.SetActive(false);
+        buttonR.SetActive(false);*/
+
+        int indexEscena = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(indexEscena);
     }
 }
--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Text Adventure es un juego creado en Unity 3D para explicar el uso de TextMeshPro, Anchors de Unity.
 
+Para probar el juego ingresa [aquí](https://datbois2pro.itch.io/text-adventure?secret=FSm3cjtOvE0HcT5krWTjYI4GKMk)
+
 ## Instalación
 
 Para descargar este proyecto necesitas descargar el siguiente comando en tu terminal.
@@ -17,3 +19,17 @@ Los requisitos para poder utilizar este repositorip
 * Unity 2021.2.5f1
 * Git
 * IDE de tu preferencia
+
+## Objetivos
+
+Los objetivos de este juego se enumeran a continuacion y se muestra su progreso.
+
+- [x] Hacer core mechanic
+- [x] Finalazar el game loop
+- [x] Crear versión web
+- [x] Publicar el juego
+- [x] Compartir el juego
+
+## Autores
+
+* André Mauricio León - Programador - [@AndreBoi02](https://github.com/AndreBoi02)
